d20dcf71138c6c0b3693ccc6289eb9d100d8c58f
Tore Hanssen <tk.hanssen@gmail.com>
2013-08-16 18:12:44 -0700

    Fixed negative number issue, commented methods
    Also fixed issue with winning when quitting

--- LevelCounter.java
+++ LevelCounter.java
@@ -1,34 +1,68 @@
+/**
+ * A {@link LevelCounter} for keeping track of levels in the game of Munchkin.
+ * Can manage your level, gear, and temporary bonus.
+ * 
+ * @author Tore Hanssen
+ */
 public class LevelCounter {
     private int level;
     private int gear;
     private int bonus;
 
+    /**
+     * Instantiates a new level counter.
+     * By default, players start at level 1.
+     */
     public LevelCounter() {
         level = 1;
         bonus = 0;
         gear = 0;
     }
 
+    /**
+     * Sets the level to the given value
+     * @param level the value to set the level to
+     */
     public void setLevel(int level) {
         this.level = level;
     }
 
+    
+    /**
+     * Increments the level.
+     */
     public void incLevel() {
         level++;
     }
 
+    /**
+     * Decrements the level.
+     */
     public void decLevel() {
         level--;
     }
 
+    /**
+     * Sets the bonus to the given value
+     * @param b the value to set the bonus to
+     */
     public void setBonus(int b) {
         bonus = b;
     }
 
+    /**
+     * @return the value of the gear
+     */
     public int getGear() {
         return gear;
     }
     
+    /**
+     * Changes the gear by the given amount.
+     * @throws IllegalArgumentException if the resulting gear is below 0.
+     * (a player cannot have negative gear, that goes in the bonus category)
+     * @param delta
+     */
     public void changeGear(int delta) {
         gear += delta;
         if (gear < 0) {
@@ -36,14 +70,26 @@ public class LevelCounter {
         }
     }
     
+    /**
+     * Changes the bonus by the given amount.
+     * @param delta the amount to change the bonus by
+     */
     public void changeBonus(int delta) {
         bonus += delta;
     }
     
+    /**
+     * Resets the bonus to its default.
+     */
     public void resetBonus() {
         bonus = 0;
     }
     
+    /**
+     * Changes the level by the given amount
+     * The level cannot go below 1.
+     * @param delta the amount to change the level by
+     */
     public void changeLevel(int delta) {
         level += delta;
         if (level < 1) {
@@ -51,15 +97,25 @@ public class LevelCounter {
         }
     }
     
+    /**
+     * @return the bonus
+     */
     public int getBonus() {
         return bonus;
     }
 
+    /**
+     * @return the level
+     */
     public int getLevel() {
         return level;
     }
 
+    /**
+     * The combat strength is defined as the total "points" accrued, including levels, bonus, and gear.
+     * @return the combat strength, or 0 if temporarily negative
+     */
     public int getCombatStrength() {
-        return level + gear - bonus;
+        return Math.max(level + gear + bonus, 0);
     }
 }
--- MunchkinLevelCounter.java
+++ MunchkinLevelCounter.java
@@ -1,7 +1,16 @@
-// Helene Martin, CSE 143
-// A basic Swing graphical user interface.  See chapter 14 of the book for more
-// on building complex user interfaces.
-import java.awt.*;
+/**
+ * This class is a command-line interactive program for tracking a player's levels in the game of
+ * Munchkin.  Players are prompted for their levels, gear, and temporary changes.
+ * Intended to be used with the game of Munchkin (this is not a stand-alone game!):
+ * http://en.wikipedia.org/wiki/Munchkin_(card_game)
+ * http://www.amazon.com/Steve-Jackson-Games-1408SJG-Munchkin/dp/1556344732/ref=sr_1_3?ie=UTF8&qid=1376701323&sr=8-3&keywords=munchkin
+ * 
+ * This {@link MunchkinLevelCounter} was inspired by the MunchkinLevelCounter app for android/iOS
+ * Munchkin is a copyright of Steve Jackson Games.
+ * This program is not affiliated in any way with Steve Jackson Games.
+ * 
+ * @author Tore Hanssen
+ */
 import java.util.Scanner;
 
 public class MunchkinLevelCounter {
@@ -25,16 +34,23 @@ public class MunchkinLevelCounter {
                 System.out.println("Command not recognized.");
             }
         }
-        if (levelTracker.getLevel() <= 10) {
+        if (levelTracker.getLevel() >= 10) {
             System.out.println("You win! Congratulations!");
         } else {
-            System.out.println("Thanks for playing " + name);
+            System.out.println("Thanks for playing " + name + ".");
             // TODO care to tell me who won and who was playing?
             
         }
 
     }
 
+    /**
+     * Prompts the user for a task.  Valid tasks include:
+     * Changing level, changing gear, changing temporary points, and resetting temporary points
+     * @param console the scanner to use
+     * @param name the name of the player
+     * @return the command the user issued
+     */
     private static String promptCommand(Scanner console, String name) {
         System.out.println("What would you like to do, " + name + "?");
         System.out.println("(type \"quit\" to quit)");
@@ -50,7 +66,12 @@ public class MunchkinLevelCounter {
     }
 
 
-
+    /**
+     * Updates the state of the game based on user input
+     * @param command the command issued by the user
+     * @param console the scanner to use for user input
+     * @param counts the {@link LevelCounter} to use
+     */
     private static void updateGame(String command, Scanner console, LevelCounter counts) {
         if (command.startsWith("l")) {
             System.out.println("How much do you want to change your level by?");
@@ -69,17 +90,15 @@ public class MunchkinLevelCounter {
             counts.changeBonus(deltaTemp);
         } else if (command.startsWith("r")) {
             counts.resetBonus();
-        } else if (command.startsWith("p")) {
-            // TODO
         }
         console.nextLine();
-        System.out.println("updated game state");
     }
 
-
-
+    /**
+     * Outputs the current state of the player's level, gear, bonus, and combat strength
+     * @param counts the player's {@link LevelCounter}
+     */
     private static void printCurrentState(LevelCounter counts) {
-        // TODO Auto-generated method stub
         System.out.println("Current Level: " + counts.getLevel());
         System.out.println("Current Gear: " + counts.getGear());
         System.out.println("Current +/-: " + counts.getBonus());
@@ -88,8 +107,11 @@ public class MunchkinLevelCounter {
         System.out.println("---------------------------");
     }
 
-
-
+    /**
+     * Prints an introductory message and gets the player's name
+     * @param console the scanner to use for user input
+     * @return the name of the player
+     */
     private static String getName(Scanner console) {
         System.out.println("Welcome to the Munchkin Level Counter!");
         System.out.println("Good luck in your game");
